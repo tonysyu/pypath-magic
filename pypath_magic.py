21e4d5b69ab5953e9fb565fd46ec9b294e1fa161
Tony S Yu <tsyu80@gmail.com>
2014-08-02 01:24:39 -0500

    Refactor pypath implementation

--- pypath_magic.py
+++ pypath_magic.py
@@ -19,13 +19,22 @@ from IPython.core.error import UsageError
 from IPython.core.magic import Magics, magics_class, line_magic
 
 
-def touch_file(path):
-    with open(path, 'w'):  # Write empty file.
-        pass
+def get_current_directory():
+    return os.getcwdu()
 
 
 def join_with_site_packages_dir(filename):
     return os.path.join(get_python_lib(), filename)
+
+
+def save_lines(filename, lines):
+    with open(filename, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+def touch_file(path):
+    with open(path, 'w'):  # Write empty file.
+        pass
 
 
 @magics_class
@@ -33,8 +42,19 @@ class PathMagic(Magics):
 
     def __init__(self, *args, **kwargs):
         super(PathMagic, self).__init__(*args, **kwargs)
+
         filename = os.environ.get('PYPATH_FILENAME', 'pypath_magic.pth')
         self.path_file = join_with_site_packages_dir(filename)
+
+        self._action_calls = {'': self._do_list_custom_paths,
+                              'a': self._do_add_path,
+                              'd': self._do_delete_path,
+                              'l': self._do_list_all_paths,
+                              'p': self._do_print_path_file}
+
+    #--------------------------------------------------------------------------
+    #  Public interface
+    #--------------------------------------------------------------------------
 
     @line_magic('pypath')
     def pypath(self, line):
@@ -58,60 +78,73 @@ class PathMagic(Magics):
         if len(opts) > 1:
             raise UsageError("%pypath: Only a single option allowed.")
 
-        action = None if len(opts) == 0 else opts.keys()[0]
+        action = '' if len(opts) == 0 else opts.keys()[0]
 
-        if action == 'l':
-            self.write_lines(sys.path)
-            return
-        elif action == 'p':
-            self.write(self.path_file)
-            return
-
-        current_dir = os.getcwdu()
-
-        with open(self.path_file, 'r') as f:
-            user_paths = f.readlines()
-        user_paths = [p.strip() for p in user_paths if p.strip()]
-
-        if action is None:
-            self.write_lines(user_paths)
-
-        elif action == 'a':  # Append current directory to user path.
-
-            if current_dir in user_paths:
-                msg = "{!r} is already in the user path."
-                self.write(msg.format(current_dir))
-                return
-
-            # Add to list for later saving and sys.path for now.
-            user_paths.append(current_dir)
-            # XXX: This doesn't add to the correct position in sys.path.
-            sys.path.append(current_dir)
-
-        elif action == 'd':  # Delete current directory from user path.
-
-            if not current_dir in user_paths:
-                msg = "{!r} is not in the user path. Cannot delete."
-                self.write(msg.format(current_dir))
-                return
-
-            # Remove from list for later saving and sys.path for now.
-            user_paths.remove(current_dir)
-            sys.path.remove(current_dir)
-
-        if action == 'a' or action == 'd':
-            with open(self.path_file, 'w') as f:
-                f.write('\n'.join(user_paths))
-            if action == 'a':
-                self.write('Added {!r} to path.'.format(current_dir))
-            else:
-                self.write('Deleted {!r} from path'.format(current_dir))
+        user_paths = self._load_user_paths()
+        return self._action_calls[action](user_paths)
 
     def write(self, line):
         print(line)
 
-    def write_lines(self, alist):
-        self.write('\n'.join(alist))
+    def write_lines(self, lines):
+        self.write('\n'.join(lines))
+
+    #--------------------------------------------------------------------------
+    #  Action interface
+    #--------------------------------------------------------------------------
+
+    def _do_add_path(self, user_paths):
+        current_dir = get_current_directory()
+        if current_dir in user_paths:
+            msg = "{!r} is already in the user path."
+            self.write(msg.format(current_dir))
+            return
+
+        self._add_path(current_dir, user_paths)
+        save_lines(self.path_file, user_paths)
+        self.write('Added {!r} to path.'.format(current_dir))
+
+    def _do_delete_path(self, user_paths):
+        current_dir = get_current_directory()
+        if not current_dir in user_paths:
+            msg = "{!r} is not in the user path. Cannot delete."
+            self.write(msg.format(current_dir))
+            return
+
+        self._delete_path(current_dir, user_paths)
+        save_lines(self.path_file, user_paths)
+        self.write('Deleted {!r} from path'.format(current_dir))
+
+    def _do_list_all_paths(self, user_paths):
+        self.write_lines(sys.path)
+
+    def _do_list_custom_paths(self, user_paths):
+        self.write_lines(self._load_user_paths())
+
+    def _do_print_path_file(self, user_paths):
+        self.write(self.path_file)
+
+    #--------------------------------------------------------------------------
+    #  Private interface
+    #--------------------------------------------------------------------------
+
+    def _load_user_paths(self):
+        with open(self.path_file, 'r') as f:
+            user_paths = f.readlines()
+        return [p.strip() for p in user_paths if p.strip()]
+
+    @staticmethod
+    def _add_path(path, user_paths):
+        """Add to list for later saving and sys.path for now."""
+        user_paths.append(path)
+        # XXX: This doesn't add to the correct position in sys.path.
+        sys.path.append(path)
+
+    @staticmethod
+    def _delete_path(path, user_paths):
+        """Delete from list for later saving and sys.path for now."""
+        user_paths.remove(path)
+        sys.path.remove(path)
 
 
 def load_ipython_extension(ipython):
